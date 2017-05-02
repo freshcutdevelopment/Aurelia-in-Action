068198451e12ddb255ea85a569bf8a652cfe63e0
Sean Hunter <sean.hunter@onset.com.au>
2017-05-03 07:00:23 +1000

    Corrected numbering on code listings

--- Chapter-3-Finish/my-books/src/app.js
+++ Chapter-3-Finish/my-books/src/app.js
@@ -1,10 +1,10 @@
 export class App {
-   configureRouter(config, router) {
-    this.router = router;
-    config.title = 'My-Books';
-    config.map([
-      { route: ['', 'home'],       name: 'home',       moduleId: 'index', nav:true, title : 'home' },
-      { route: 'books',            name: 'books',      moduleId: './resources/elements/books',   nav: true, title : 'books' },
-    ]);
-   }
+  configureRouter(config, router) {
+  this.router = router;
+  config.title = 'My-Books';
+  config.map([ 
+   { route: ['', 'home'], name: 'home', moduleId: 'index' },
+   { route: 'books', name: 'books', moduleId: './resources/elements/books'},
+  ]);
+  }
 }
