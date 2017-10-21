97b394e53c265111cf3fe6e1d5622322192c8c2b
Sean Hunter <sean.hunter@onset.com.au>
2017-10-21 15:40:10 +1100

    Added chapter 15 listings

---
+++ Chapter-15/Listing_15.7_app.po.js
@@ -0,0 +1,7 @@
+export class PageObjectApp {
+    constructor() {}
+  
+    getCurrentPageTitle() {
+      return browser.getTitle();
+    }
+  }
---
+++ Chapter-15/Listing_15.8_login.po.js
@@ -0,0 +1,28 @@
+export class PageObjectLogin {
+    constructor() {}
+  
+    getHeader() {
+      return element(by.tagName("h1.brand-heading")).getText();
+    }
+  
+    setUsername(value) {
+      return element(by.valueBind("userName"))
+        .clear()
+        .sendKeys(value);
+    }
+  
+    setPassword(value) {
+      return element(by.valueBind("password"))
+        .clear()
+        .sendKeys(value);
+    }
+  
+    pressSubmitButton() {
+      return element(by.css('button[type="submit"]')).click();
+    }
+  
+    getLoginError() {
+      return element(by.css(".card-body.login-error")).getText();
+    }
+  }
+  
