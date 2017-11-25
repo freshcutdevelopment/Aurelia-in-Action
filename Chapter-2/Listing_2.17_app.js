1843f796a4dacd69c998cb518da53c1ccae4844e
Maik Derstappen <md@derico.de>
2017-11-25 20:01:52 +0200

    fix path of module index

--- Chapter-2/Listing_2.17_app.js
+++ Chapter-2/Listing_2.17_app.js
@@ -3,7 +3,7 @@ export class App {
   this.router = router;
   config.title = 'My-Books';
   config.map([ 
-   { route: ['', 'home'], name: 'home', moduleId: 'index' },
+   { route: ['', 'home'], name: 'home', moduleId: './resources/elements/index' },
    { route: 'books', name: 'books', moduleId: './resources/elements/books'},
   ]);
   }
